79152a1101fa7d84c1728467844df59518136cfc
mandu <wbslalswns@naver.com>
2024-07-10 09:33:43 +0900

    ADD get prime factors code and test code

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,14 @@
 class PrimeFactor:
-    pass
+
+    def get_prime_numbers_from_2_to_number(self, number):
+        is_prime = [True] * (number + 1)
+        is_prime[0], is_prime[1] = False, False
+        for i in range(2, int(number ** (1 / 2)) + 1):
+            if is_prime[i]:
+                for j in range(i * i, number + 1, i):
+                    is_prime[j] = False
+        return [num for num, prime in enumerate(is_prime) if prime]
+    def get_prime_factors(self, number) -> []:
+        prime_numbers = self.get_prime_numbers_from_2_to_number(number)
+        return [prime_number for prime_number in prime_numbers if number % prime_number == 0]
+
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -4,6 +4,27 @@ from prime_factors import PrimeFactor
 
 
 class TestPrimeFactor(TestCase):
+    def setUp(self):
+        self.prime_factor = PrimeFactor()
+
+    def test_get_prime_numbers_from_2_to_20(self):
+        self.assertEqual([2, 3, 5, 7, 11, 13, 17, 19],
+                         self.prime_factor.get_prime_numbers_from_2_to_number(20))
+
     def test_prime_factor_of_1(self):
-        prime_factor = PrimeFactor()
-        self.assertEqual([1], [1])
+        self.assertEqual([], self.prime_factor.get_prime_factors(1))
+
+    def test_prime_factor_of_2(self):
+        self.assertEqual([2], self.prime_factor.get_prime_factors(2))
+
+    def test_prime_factor_of_5(self):
+        self.assertEqual([5], self.prime_factor.get_prime_factors(5))
+
+    def test_prime_factor_of_10(self):
+        self.assertEqual([2, 5], self.prime_factor.get_prime_factors(10))
+
+    def test_prime_factor_of_15(self):
+        self.assertEqual([3, 5], self.prime_factor.get_prime_factors(15))
+
+    def test_prime_factor_of_20(self):
+        self.assertEqual([2, 5], self.prime_factor.get_prime_factors(20))
